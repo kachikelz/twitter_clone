db9b14fc2ac0e98a4662400d454e079fc51112f3
kachikelz <kevonyedikachi@gmail.com>
2023-04-18 20:38:18 +0100

    sign in update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -99,7 +99,7 @@ const Home: NextPage = () => {
       <main className="flex  h-screen justify-center">
         <div className="h-full w-full border-x border-slate-400 md:max-w-2xl">
           <div className="flex border-b border-slate-400 p-4">
-            {/* <SignedIn>
+            <SignedIn>
               <div onClick={() => <SignOutButton />} className="w-full">
                 <CreatePostWizard />
               </div>
@@ -108,14 +108,14 @@ const Home: NextPage = () => {
 
             <SignedOut>
               <SignInButton />
-            </SignedOut> */}
-            {!isSignedIn && (
+            </SignedOut>
+            {/* {!isSignedIn && (
               <div className="flex justify-center">
                 {" "}
                 <SignInButton />{" "}
               </div>
             )}
-            {isSignedIn && <CreatePostWizard />}
+            {isSignedIn && <CreatePostWizard />} */}
           </div>
 
           <Feed />
